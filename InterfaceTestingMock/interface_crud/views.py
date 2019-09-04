0fc0ddcd965081686b53b657d63a26f2d06cb6c9
Crisimple <1240281595@qq.com>
2019-09-05 00:05:53 +0800

    InterfaceTestingMock add user_auth

--- InterfaceTestingMock/interface_crud/views.py
+++ InterfaceTestingMock/interface_crud/views.py
@@ -22,76 +22,88 @@ def query_article(request):
 
 # 增加文章
 def add_article(request):
-    if request.method == "POST":
-        # b''
-        print('request.body: ', request.body)
-        print('request.body: ', type(request.body))
-        req_dict = json.loads(request.body)
-        print('req_json: ', req_dict)
-        print('req_json: ', type(req_dict))
-        key_flag = req_dict.get('title') and req_dict.get('content') and len(req_dict) == 2
-        print('key_flag: ', key_flag)
-        # 判断请求体是否正确
-        if key_flag:
-            title = req_dict['title']
-            content = req_dict['content']
-            # title返回的是一个list
-            title_exist = Article.objects.filter(title=title)
-            # 判断是否存在同名的title
-            if len(title_exist) != 0:
-                return JsonResponse({"status": "BS.400", "msg": "title already exist, fail to publish."})
-            """
-            插入数据
-            """
-            add_art = Article(title=title, content=content, status='alive')
-            add_art.save()
-            return HttpResponse(add_art)
-            return JsonResponse({"status": "BS.200", "msg": "add article success."})
-        else:
-            return JsonResponse({"status": "BS.400", "message": "please check param."})
+    auth_res = user_auth(request)
+    if auth_res == "auth_fail":
+        return JsonResponse({"status": "BS.401", "msg": "user auth failed."})
     else:
-        return HttpResponse("方法错误，你应该使用POST请求方式")
+        if request.method == "POST":
+            # b''
+            print('request.body: ', request.body)
+            print('request.body: ', type(request.body))
+            req_dict = json.loads(request.body)
+            print('req_json: ', req_dict)
+            print('req_json: ', type(req_dict))
+            key_flag = req_dict.get('title') and req_dict.get('content') and len(req_dict) == 2
+            print('key_flag: ', key_flag)
+            # 判断请求体是否正确
+            if key_flag:
+                title = req_dict['title']
+                content = req_dict['content']
+                # title返回的是一个list
+                title_exist = Article.objects.filter(title=title)
+                # 判断是否存在同名的title
+                if len(title_exist) != 0:
+                    return JsonResponse({"status": "BS.400", "msg": "title already exist, fail to publish."})
+                """
+                插入数据
+                """
+                add_art = Article(title=title, content=content, status='alive')
+                add_art.save()
+                return HttpResponse(add_art)
+                return JsonResponse({"status": "BS.200", "msg": "add article success."})
+            else:
+                return JsonResponse({"status": "BS.400", "message": "please check param."})
+        else:
+            return HttpResponse("方法错误，你应该使用POST请求方式")
 
 
 # 更新文章
 def modify_article(request, article_id):
-    if request.method == 'POST':
-        modify_req = json.loads(request.body)
-        try:
-            article = Article.objects.get(id=article_id)
-            print("article", article)
-            key_flag = modify_req.get('title') and modify_req.get('content') and len(modify_req) == 2
-            if key_flag:
-                title = modify_req['title']
-                content = modify_req['content']
-                title_exist = Article.objects.filter(title=title)
-                if len(title_exist) > 1:
-                    return JsonResponse({"status": "BS.400", "msg": "title already exist."})
-
-                # 更新文章
-                old_article = Article.objects.get(id=article_id)
-                old_article.title = title
-                old_article.content = content
-                old_article.save()
-                return JsonResponse({"status": "BS.200", "msg": "modify article sucess."})
-        except Article.DoesNotExist:
-            return JsonResponse({"status": "BS.300", "msg": "article is not exists,fail to modify."})
+    auth_res = user_auth(request)
+    if auth_res == "auth_fail":
+        return JsonResponse({"status": "BS.401", "msg": "user auth failed."})
     else:
-        return HttpResponse("方法错误，你应该使用POST请求方式")
+        if request.method == 'POST':
+            modify_req = json.loads(request.body)
+            try:
+                article = Article.objects.get(id=article_id)
+                print("article", article)
+                key_flag = modify_req.get('title') and modify_req.get('content') and len(modify_req) == 2
+                if key_flag:
+                    title = modify_req['title']
+                    content = modify_req['content']
+                    title_exist = Article.objects.filter(title=title)
+                    if len(title_exist) > 1:
+                        return JsonResponse({"status": "BS.400", "msg": "title already exist."})
+
+                    # 更新文章
+                    old_article = Article.objects.get(id=article_id)
+                    old_article.title = title
+                    old_article.content = content
+                    old_article.save()
+                    return JsonResponse({"status": "BS.200", "msg": "modify article sucess."})
+            except Article.DoesNotExist:
+                return JsonResponse({"status": "BS.300", "msg": "article is not exists,fail to modify."})
+        else:
+            return HttpResponse("方法错误，你应该使用POST请求方式")
 
 
 # 删除文章
 def delete_article(request, article_id):
-    if request.method == 'DELETE':
-        try:
-            article = Article.objects.get(id=article_id)
-            article_id = article.id
-            article.delete()
-            return JsonResponse({"status": "BS.200", "msg": "delete article success."})
-        except Article.DoesNotExist:
-            return JsonResponse({"status": "BS.300", "msg": "article is not exists,fail to delete."})
+    auth_res = user_auth(request)
+    if auth_res == "auth_fail":
+        return JsonResponse({"status": "BS.401", "msg": "user auth failed."})
     else:
-        return HttpResponse("方法错误，你应该使用DELETE请求方式")
+        if request.method == 'DELETE':
+            try:
+                article = Article.objects.get(id=article_id)
+                article_id = article.id
+                article.delete()
+                return JsonResponse({"status": "BS.200", "msg": "delete article success."})
+            except Article.DoesNotExist:
+                return JsonResponse({"status": "BS.300", "msg": "article is not exists,fail to delete."})
+        else:
+            return HttpResponse("方法错误，你应该使用DELETE请求方式")
 
 
 def test_api(request):
@@ -103,5 +115,17 @@ def test_api(request):
         return HttpResponse("方法错误")
 
 
-# 鉴权
+# 用户认证
 # 四个简单的接口已经可以运行了，但是在发请求之前没有进行鉴权，毫无安全性可言。下面来实现简单的认证机制。需要用到内建模块hashlib，hashlib提供了常见的摘要算法，如MD5，SHA1等。
+def user_auth(request):
+    token = request.META.get("HTTP_X_TOKEN", b'')
+    print("token: ", token)
+    if token:
+        # 暂时写上 auth 接口返回的数据
+        if token == '0a6db4e59c7fff2b2b94a297e2e5632e':
+            return "auth_success"
+        else:
+            return "auth_fail"
+    else:
+        return "auth_fail"
+
--- InterfaceTestingMock/tests/interface_crud_tests.py
+++ InterfaceTestingMock/tests/interface_crud_tests.py
@@ -12,44 +12,66 @@
 import requests
 import unittest
 
+
 class TestInterfaceCrud(unittest.TestCase):
-    @unittest.skip("跳过测试")
+    @unittest.skip("跳过 test_query_article 测试")
     def test_query_article(self):
         payload = {}
         res = requests.get('http://127.0.0.1:8000/query_article/', params=payload)
         print("test_query_article: ", res.text)
 
-    @unittest.skip("跳过测试")
+    @unittest.skip("跳过 test_add_article 测试")
     def test_add_article(self):
         payload = {
-            "title": "title8",
-            "content": "content8",
+            "title": "title5",
+            "content": "content5",
         }
-        res = requests.post('http://127.0.0.1:8000/add_article/', json=payload)
+        Headers = {
+            # "Authorization": '通用的token，但是该接口使用的是X-Token',
+            "Content-Type": "application/json; charset=utf-8",
+            "Accept": "application/json",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3730.400 QQBrowser/10.5.3805.400",
+            "X-Token": "0a6db4e59c7fff2b2b94a297e2e5632e"
+        }
+        res = requests.post('http://127.0.0.1:8000/add_article/', headers=Headers, json=payload)
         print(res.request)
         print(res.text)
 
-    @unittest.skip("跳过测试")
+    @unittest.skip("跳过 test_modify_article 测试")
     def test_modify_article(self):
         payload = {
             "title": "title1",
             "content": "content1",
         }
-        res = requests.post('http://127.0.0.1:8000/modify_article/1', json=payload)
+        Headers = {
+            # "Authorization": '通用的token，但是该接口使用的是X-Token',
+            "Content-Type": "application/json; charset=utf-8",
+            "Accept": "application/json",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3730.400 QQBrowser/10.5.3805.400",
+            "X-Token": "0a6db4e59c7fff2b2b94a297e2e5632e"
+        }
+        res = requests.post('http://127.0.0.1:8000/modify_article/1', headers=Headers, json=payload)
         print(res.request)
         print(res.text)
 
-    # @unittest.skip("跳过测试")
+    # @unittest.skip("跳过 test_delete_article 测试")
     def test_delete_article(self):
         payload = {
             "title": "title2",
             "content": "content2",
         }
-        res = requests.delete('http://127.0.0.1:8000/delete_article/2', json=payload)
+        Headers = {
+            # "Authorization": '通用的token，但是该接口使用的是X-Token',
+            "Content-Type": "application/json; charset=utf-8",
+            "Accept": "application/json",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3730.400 QQBrowser/10.5.3805.400",
+            "X-Token": "0a6db4e59c7fff2b2b94a297e2e5632e"
+        }
+        res = requests.delete('http://127.0.0.1:8000/delete_article/2', headers=Headers, json=payload)
         print(res.request)
         print(res.text)
 
-    @unittest.skip("跳过测试")
+    @unittest.skip("跳过 test_test_api 测试")
     def test_test_api(self):
         payload = {
             'title': 'title1',
