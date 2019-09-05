cbd377a315a7ca660ab032bc1b17b5e52cdda021
crisimple <crisimple@foxmail.com>
2019-09-05 12:40:18 +0800

    util.operate_json.py

---
+++ util/operate_json.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   operate_json.py
+@Time    :   2019/9/5 12:24
+@Author  :   Crisimple
+@Github :    https://crisimple.github.io/
+@Contact :   Crisimple@foxmail.com
+@License :   (C)Copyright 2017-2019, Micro-Circle
+@Desc    :   操作 JSON 文件中的数据
+"""
+import json
+
+
+class OperateJson(object):
+    def __init__(self, file_name=None):
+        if file_name:
+            self.file_name = file_name
+        else:
+            self.file_name = '../data/util_data/operate_json.json'
+
+        self.data = self.get_json()
+
+    # 读取 json 文件
+    def get_json(self):
+        with open(self.file_name, encoding='utf-8') as fp:
+            data = json.load(fp)
+        return data
+
+    # 根据关键词读取数据
+    def get_key_data(self, key):
+        return self.data[key]
+
+
+if __name__ == '__main__':
+    oj = OperateJson()
+    print('login: ', oj.get_key_data("login"))
+    print('login.username: ', oj.get_key_data("login")["username"])
+    print('login.password: ', oj.get_key_data("login")["username"])
+    print('logout: ', oj.get_key_data("logout"))
+    print('logout.code: ', oj.get_key_data("logout")["code"])
+    print('logout.info: ', oj.get_key_data("logout")["info"])
