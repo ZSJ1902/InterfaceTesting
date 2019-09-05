a81f236e999644654554c3cb65566000f3f6f302
crisimple <crisimple@foxmail.com>
2019-09-05 18:43:19 +0800

    util.operate_mysql.py

---
+++ util/operate_mysql.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   operate_mysql.py
+@Time    :   2019/9/5 16:10
+@Author  :   Crisimple
+@Github :    https://crisimple.github.io/
+@Contact :   Crisimple@foxmail.com
+@License :   (C)Copyright 2017-2019, Micro-Circle
+@Desc    :   操作 数据库 中的数据
+"""
+
+import pymysql
+import json
+
+
+class OperateMysql(object):
+    def __init__(self):
+        # 数据库初始化连接
+        self.connect_interface_testing = pymysql.connect(
+            "129.28.170.125",
+            "root",
+            "Root@159357",
+            "InterfaceTesting",
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+        # 创建游标操作数据库
+        self.cursor_interface_testing = self.connect_interface_testing.cursor()
+
+    def select_data(self, sql):
+        # 执行 sql 语句
+        self.cursor_interface_testing.execute(sql)
+        # 获取查询到的第一条数据
+        first_data = self.cursor_interface_testing.fetchone()
+        # 将返回结果转换成 str 数据格式
+        first_data = json.dumps(first_data)
+        return first_data
+
+
+if __name__ == "__main__":
+    om = OperateMysql()
+    res = om.select_data(
+        """
+            SELECT * FROM test_table;
+        """
+    )
+    print(res)
