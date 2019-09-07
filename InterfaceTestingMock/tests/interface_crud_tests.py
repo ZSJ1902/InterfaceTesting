5b3a011ff2c2c0cf97a1a91dc83abfb95609cc8d
crisimple <crisimple@foxmail.com>
2019-09-07 18:28:32 +0800

    tests.run_excel_testcases.py

--- InterfaceTestingMock/tests/interface_crud_tests.py
+++ InterfaceTestingMock/tests/interface_crud_tests.py
@@ -41,8 +41,8 @@ class TestInterfaceCrud(unittest.TestCase):
     @unittest.skip("跳过 test_modify_article 测试")
     def test_modify_article(self):
         payload = {
-            "title": "title1",
-            "content": "content1",
+            "title": "title6_m",
+            "content": "content6_m",
         }
         headers = {
             # "Authorization": '通用的token，但是该接口使用的是X-Token',
@@ -58,8 +58,8 @@ class TestInterfaceCrud(unittest.TestCase):
     @unittest.skip("跳过 test_delete_article 测试")
     def test_delete_article(self):
         payload = {
-            "title": "title2",
-            "content": "content2",
+            "title": "title7",
+            "content": "content7",
         }
         headers = {
             # "Authorization": '通用的token，但是该接口使用的是X-Token',
@@ -68,7 +68,7 @@ class TestInterfaceCrud(unittest.TestCase):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3730.400 QQBrowser/10.5.3805.400",
             "X-Token": "0a6db4e59c7fff2b2b94a297e2e5632e"
         }
-        res = requests.delete('http://127.0.0.1:8000/delete_article/2', json=payload, headers=headers)
+        res = requests.delete('http://127.0.0.1:8000/delete_article/7', json=payload, headers=headers)
         print(res.request)
         print(res.text)
 
